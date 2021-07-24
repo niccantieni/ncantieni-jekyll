0a93f1a334a5f1b87dd2aad74577c5a90d6863dc
Nic Cantieni <nic.cantieni@bluewin.ch>
2021-07-24 10:39:52 +0200

    automated update

--- _site/deploy-minimal.sh
+++ _site/deploy-minimal.sh
@@ -2,4 +2,4 @@ jekyll build
 git add .
 git commit -am "automated update"
 git push github master
-ssh ncantieni@slab1.ethz.ch 'cd homepage; cd download; svn export https://github.com/niccantieni/ncantieni-jekyll/trunk/_site/download/ ./ --force; cd ../digitech; svn export https://github.com/niccantieni/ncantieni-jekyll/trunk/_site/digitech/ ./ --force; cd ..; svn export https://github.com/niccantieni/ncantieni-jekyll/trunk/_site/index.html ./ --force;exit;'
+ssh ncantieni@slab1.ethz.ch 'cd homepage; exit;'
